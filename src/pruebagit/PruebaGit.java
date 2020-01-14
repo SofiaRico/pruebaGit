e2abf21aa9b5202a074e7b02da910dd0cdff9710
xp <xp@CETYSDAM00>
2020-01-14 20:10:15 +0100

    Añadido el método muyVanidoso

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -10,12 +10,20 @@ package pruebagit;
  * @author xp
  */
 public class PruebaGit {
+    public boolean muyVanidoso (int numero){
+    if(numero %11==0|| (numero-1)%11==0){
+        return true;
+    }
+    return false;
+}
+}
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba GIT");
-    }
+       //https://github.com/SofiaRico/pruebaGit.git
+       //He añadido un método que quita espacios
+       
     
 }
